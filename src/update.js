fc2ab0d62950a8931b3d42a7d5a524fbd8c1d143
Uri Shaked <uri@urishaked.com>
2022-01-07 00:13:08 +0200

    feat: add types

--- src/update.js
+++ src/update.js
@@ -4,9 +4,33 @@
 import { writeFileSync } from 'fs';
 import fetch from 'node-fetch';
 
+/**
+ * @typedef {{
+ *   name: string;
+ *   version: string;
+ *   author: string;
+ *   maintainer: string;
+ *   sentence: string;
+ *   paragraph: string;
+ *   website: string;
+ *   category: string;
+ *   architectures: string[];
+ *   types: string[];
+ *   repository: string;
+ *   url: string;
+ *   archiveFileName: string;
+ *   size: number;
+ *   checksum: string;
+ * }} ILibrariesEntry
+ */
+
+/**
+ * @typedef {{  libraries: ILibrariesEntry[] }} ILibraryJson
+ */
+
 async function main() {
   const req = await fetch('http://downloads.arduino.cc/libraries/library_index.json');
-  const { libraries } = await req.json();
+  const { libraries } = /** @type {ILibraryJson} */ (await req.json());
   const result = {
     libraries: Array.from(new Set(libraries.map((lib) => lib.name))).sort(),
   };
